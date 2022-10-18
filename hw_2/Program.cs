bb0a7d4b8aa18f55350b9084f01507ad68cd77fc
Goncharenya <kotmay@mail.ru>
2022-10-18 14:04:25 +0300

    task2_hw2

--- hw_2/Program.cs
+++ hw_2/Program.cs
@@ -1,9 +1,6 @@
-﻿/* Задача 10: Напишите программу, которая принимает на вход 
+﻿/* Задача 10
+Напишите программу, которая принимает на вход 
 трёхзначное число и на выходе показывает вторую цифру этого числа.
-
-456 -> 5
-782 -> 8
-918 -> 1
 */
 
 /*
@@ -12,9 +9,30 @@ int CutNum(int number)
     int result = (number/10) % 10;
     return result;
 }
-
 int randomNum = new Random().Next(100, 1000);
 int resultNum = CutNum(randomNum);
 
 Console.WriteLine($"новая версия числа {randomNum} следующая: {resultNum}");
 */
+
+/*Задача 13
+Напишите программу, которая выводит третью цифру заданного 
+числа или сообщает, что третьей цифры нет.
+*/
+
+Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 99)
+{
+    Console.WriteLine("упс ... а неть :)");
+}
+if (number > 100 && number < 1000)
+{
+    int number1 = number%10;
+    Console.WriteLine(number1);
+}
+if (number > 1000)
+{
+    int number2 = (number%100) /10;
+    Console.WriteLine(number2);
+}
