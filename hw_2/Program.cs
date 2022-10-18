e17cb565d4c738809069e3ab83e4ae9902358458
Goncharenya <kotmay@mail.ru>
2022-10-18 13:19:42 +0300

    task1_hw2

---
+++ hw_2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+/*
+int CutNum(int number)
+{
+    int result = (number/10) % 10;
+    return result;
+}
+
+int randomNum = new Random().Next(100, 1000);
+int resultNum = CutNum(randomNum);
+
+Console.WriteLine($"новая версия числа {randomNum} следующая: {resultNum}");
+*/
