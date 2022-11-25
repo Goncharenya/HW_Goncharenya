1c3a604c9c415f01fcaf0134c10ca6430142f53b
Goncharenya <kotmay@mail.ru>
2022-11-25 16:20:09 +0300

    hw_9

---
+++ hw_9/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// void ShowNumbers(int num)
+// {
+//     if (num > 0)
+//     {
+//          Console.Write(num + " ");
+//          ShowNumbers(num - 1);
+//        }
+// }
+// ShowNumbers(5);
+
+
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+// int SumNumbers(int m, int n)
+// {
+//     if (m == 0)
+//         return (n * (n + 1)) / 2;
+//     else if (n == 0)
+//         return (m * (m + 1)) / 2;
+//     else if (m == n)
+//         return m;
+//     else if (m < n)
+//         return n + SumNumbers(m, n - 1);
+//     else
+//         return n + SumNumbers(m, n + 1);
+// }
+
+
+Console.Write("input the m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("input the n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+//  Console.Write(SumNumbers(m, n));
+
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
---
+++ lesson_9/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+// void ShowNums(int n)
+// {
+//     if (n > 1) ShowNums(n - 1);
+//     Console.Write(n + " ");
+// }
+
+// ShowNums(5);
+
+//Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+// int Sum(int n)
+// {
+//   if(n != 0) return Sum(n / 10) + n % 10;
+// return 0;
+// }
+// Console.Write(Sum(1525));
+
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// void ShowNums(int m, int n)
+// {
+//     if (m < n)
+//     {
+//          Console.Write($"{m}");
+//         ShowNums(m + 1, n);
+//     }
+//     else if (m > n)
+//     {
+//         Console.Write($"{n}");
+//         ShowNums(m, n+1);
+//     }
+// }
+// Console.Write("input n: ");
+// int n = Convert.ToInt32(Console.Read());
+// Console.Write("input m: ");
+// int m = Convert.ToInt32(Console.Read());
+//ShowNums(1, 8);
+
+// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B.
+
+double NumsPow(double num, double pow)
+{
+    if (pow > 0)
+        return NumsPow(num, pow - 1) * num;
+
+    else if (pow < 0)
+        return NumsPow(num, pow + 1) / num;
+    return 1;
+}
+
+Console.Write(NumsPow(2, 2));
